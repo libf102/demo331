8fec0a942dd51cebd38dde38250bfb9825072d1e
李京 <lij102102@gmail.com>
2020-03-31 21:56:27 +0800

    老许第一次提交代码

---
+++ src/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("老许第一次提交");
+        System.out.println("老许第一天上班提交");
+        System.out.println("老许哈哈大笑");
+
+    }
+}
